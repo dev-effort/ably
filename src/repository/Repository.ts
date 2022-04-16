3f122c49dbb2705e3a802865822d53cd672a245a
seungjun_kim <dregonc89@gmail.com>
2022-04-16 17:35:45 +0900

    refactor: refactoring util axios
    change util axios implements from class to literal object

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -10,67 +10,62 @@ import {
   UpdatePasswordOutDto,
   UserOutDto,
 } from '@src/types';
-import Axios from '@utils/Axios';
+import APIs from '@utils/Axios';
 import { getCookie } from '@utils/cookie';
 
 class Repository {
-  API: Axios;
-
-  constructor() {
-    this.API = new Axios();
-  }
-
-  async postLogin(dto: LoginInDto): Promise<LoginOutDto> {
+  public static async postLogin(dto: LoginInDto): Promise<LoginOutDto> {
     const params = new URLSearchParams();
     params.append('email', dto.email);
     params.append('password', dto.password);
 
-    const result = await this.API.post('/login', params);
+    const result = await APIs.post('/login', params);
+    console.log('result', result);
 
     return result;
   }
 
-  async postLogout(): Promise<LogoutOutDto> {
-    const result = await this.API.post('/logout', undefined, {
+  public static async postLogout(): Promise<LogoutOutDto> {
+    const result = await APIs.post('/logout', undefined, {
       headers: { Authorization: `Bearer ${getCookie('accessToken')}` },
     });
 
     return result;
   }
 
-  async getUser(): Promise<UserOutDto> {
-    const result = await this.API.get('/user', {
+  public static async getUser(): Promise<UserOutDto> {
+    const result = await APIs.get('/user', {
       headers: { Authorization: `Bearer ${getCookie('accessToken')}` },
     });
 
     return result;
   }
 
-  async getAuthCode(dto: AuthCodeInDto): Promise<AuthCodeOutDto> {
-    const result = await this.API.get(`/reset-password?email=${dto.email}`);
+  public static async getAuthCode(dto: AuthCodeInDto): Promise<AuthCodeOutDto> {
+    const result = await APIs.get(`/reset-password?email=${dto.email}`);
     return result;
   }
 
-  async postConfirmToken(dto: ConfirmTokenInDto): Promise<ConfirmTokenOutDto> {
+  public static async postConfirmToken(dto: ConfirmTokenInDto): Promise<ConfirmTokenOutDto> {
     const params = new URLSearchParams();
     params.append('email', dto.email);
     params.append('authCode', dto.authCode);
     params.append('issueToken', dto.issueToken);
 
-    const result = await this.API.post('/reset-password', params);
+    const result = await APIs.post('/reset-password', params);
     return result;
   }
 
-  async updatePassword(dto: UpdatePasswordInDto): Promise<UpdatePasswordOutDto> {
+  public static async updatePassword(dto: UpdatePasswordInDto): Promise<UpdatePasswordOutDto> {
     const params = new URLSearchParams();
     params.append('email', dto.email);
     params.append('confirmToken', dto.confirmToken);
     params.append('newPassword', dto.newPassword);
     params.append('newPasswordConfirm', dto.newPasswordConfirm);
 
-    const result = await this.API.patch('/reset-password', params);
+    const result = await APIs.patch('/reset-password', params);
     return result;
   }
 }
 
-export default new Repository();
+export default Repository;
--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -1,60 +1,64 @@
 import axios, { AxiosError, AxiosInstance } from 'axios';
 
-class Axios {
+interface AxiosType {
   instance: AxiosInstance;
-
-  constructor() {
-    this.instance = axios.create();
-    this.instance.defaults.baseURL = 'https://ably-frontend-assignment-server.vercel.app/api';
-    this.instance.defaults.headers.common['Content-Type'] = 'applcation/json';
-
-    this.instance.interceptors.response.use(
-      response => {
-        return response.data;
-      },
-      (error: AxiosError) => {
-        if (error.response) {
-          // 요청이 이루어쪗으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답
-          console.error('http error status: ', error.response.status, ', data: ', error.response.data);
-        } else if (error.request) {
-          // 요청이 이루어 졌으나 응답을 받지 못함
-          console.error(
-            'server is not responding. server is not valid or invalid path or check network.',
-            error.request,
-          );
-        } else {
-          // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생
-          console.error('Error', error);
-        }
-        return Promise.reject(error);
-      },
-    );
-  }
-
-  public async get(url: string, config = {}) {
-    const response: any = await this.instance.get(url, config);
-    return response;
-  }
-
-  public async post(url: string, data = {}, config = {}) {
-    const response: any = await this.instance.post(url, data, config);
-    return response;
-  }
-
-  public async delete(url: string, config = {}) {
-    const response: any = await this.instance.delete(url, config);
-    return response;
-  }
-
-  public async put(url: string, data = {}, config = {}) {
-    const response: any = await this.instance.put(url, data, config);
-    return response;
-  }
-
-  public async patch(url: string, data = {}, config = {}) {
-    const response: any = await this.instance.patch(url, data, config);
-    return response;
-  }
+  get: (url: string, config?: any) => Promise<any>;
+  post: (url: string, data?: any, config?: any) => Promise<any>;
+  delete: (url: string, config?: any) => Promise<any>;
+  put: (url: string, data?: any, config?: any) => Promise<any>;
+  patch: (url: string, data?: any, config?: any) => Promise<any>;
 }
 
-export default Axios;
+const axiosInstance = axios.create();
+axiosInstance.defaults.baseURL = 'https://ably-frontend-assignment-server.vercel.app/api';
+axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+
+axiosInstance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  (error: AxiosError) => {
+    if (error.response) {
+      // 요청이 이루어쪗으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답
+      console.error('http error status: ', error.response.status, ', data: ', error.response.data);
+    } else if (error.request) {
+      // 요청이 이루어 졌으나 응답을 받지 못함
+      console.error('server is not responding. server is not valid or invalid path or check network.', error.request);
+    } else {
+      // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생
+      console.error('Error', error);
+    }
+    return Promise.reject(error);
+  },
+);
+
+const APIs: AxiosType = {
+  instance: axiosInstance,
+
+  async get(url: string, config = {}) {
+    const response = await this.instance.get(url, config);
+    return response;
+  },
+
+  async post(url: string, data = {}, config = {}) {
+    const response = await this.instance.post(url, data, config);
+    return response;
+  },
+
+  async put(url: string, data = {}, config = {}) {
+    const response = await this.instance.put(url, data, config);
+    return response;
+  },
+
+  async delete(url: string, config = {}) {
+    const response = await this.instance.delete(url, config);
+    return response;
+  },
+
+  async patch(url: string, data = {}, config = {}) {
+    const response = await this.instance.patch(url, data, config);
+    return response;
+  },
+};
+
+export default APIs;
