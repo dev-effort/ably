fffc24c66a7b80c25f07f3dad02ef0ed4eeceda8
seungjun_kim <dregonc89@gmail.com>
2022-04-16 12:58:44 +0900

    feat: add cookie util
    create cookie util methods
    setCookie, getCookie, removeCookie

---
+++ src/utils/cookie.ts
@@ -0,0 +1,15 @@
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const setCookie = (key: string, value: string, option?: any) => {
+  return cookies.set(key, value, { ...option });
+};
+
+export const getCookie = (key: string) => {
+  return cookies.get(key);
+};
+
+export const removeCookie = (key: string) => {
+  return cookies.remove(key);
+};
