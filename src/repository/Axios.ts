f98ea7ca599bab8dbb2ece7194582d8155c570cd
seungjun_kim <dregonc89@gmail.com>
2022-04-16 10:39:02 +0900

    feat: add axios http error handling
    fix: obj of axios interceptors change from axios to this.instance
    add: write interceptors error handling

--- src/repository/Axios.ts
+++ src/repository/Axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 
 class Axios {
   instance: AxiosInstance;
@@ -8,11 +8,24 @@ class Axios {
     this.instance.defaults.baseURL = 'https://ably-frontend-assignment-server.vercel.app/';
     this.instance.defaults.headers.common['Content-Type'] = 'applcation/json';
 
-    axios.interceptors.response.use(
+    this.instance.interceptors.response.use(
       response => {
-        return response;
+        return response.data;
       },
-      error => {
+      (error: AxiosError) => {
+        if (error.response) {
+          // 요청이 이루어쪗으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답
+          console.error('http error status: ', error.response.status, ', data: ', error.response.data);
+        } else if (error.request) {
+          // 요청이 이루어 졌으나 응답을 받지 못함
+          console.error(
+            'server is not responding. server is not valid or invalid path or check network.',
+            error.request,
+          );
+        } else {
+          // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생
+          console.error('Error', error);
+        }
         return Promise.reject(error);
       },
     );
